a626aeee6b87f327e423caa227ab679532bb11f6
cydercode <cydercode@gmail.com>
2026-01-30 00:27:45 +0100

    Activation success email

--- src/main/java/com/cydercode/controller/EmailVerificationController.java
+++ src/main/java/com/cydercode/controller/EmailVerificationController.java
@@ -3,6 +3,7 @@ package com.cydercode.controller;
 import com.cydercode.dto.emailVerification.VerifyEmailRequest;
 import com.cydercode.dto.emailVerification.VerifyEmailResponse;
 import com.cydercode.service.EmailVerificationService;
+import com.mailjet.client.errors.MailjetException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class EmailVerificationController {
     private final EmailVerificationService emailVerificationService;
 
     @PostMapping("/verify-email")
-    public VerifyEmailResponse verifyEmail(@Valid @RequestBody VerifyEmailRequest request) {
+    public VerifyEmailResponse verifyEmail(@Valid @RequestBody VerifyEmailRequest request) throws MailjetException {
         emailVerificationService.verifyEmail(request.getVerificationToken());
         return new VerifyEmailResponse();
     }
--- src/main/java/com/cydercode/service/EmailVerificationService.java
+++ src/main/java/com/cydercode/service/EmailVerificationService.java
@@ -2,7 +2,10 @@ package com.cydercode.service;
 
 import com.cydercode.model.Account;
 import com.cydercode.repository.AccountsRepository;
-import java.util.Optional;
+import com.cydercode.service.email.MailService;
+import com.mailjet.client.errors.MailjetException;
+import jakarta.transaction.Transactional;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,20 +15,34 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class EmailVerificationService {
 
-    private final AccountsRepository accountsRepository;
+  private final AccountsRepository accountsRepository;
+  private final MailService mailService;
 
-    public void verifyEmail(String emailVerificationToken) {
-        log.info("Verifying email with token: {}", emailVerificationToken);
-        Account account = accountsRepository.findByEmailVerificationToken(emailVerificationToken)
-                .orElseThrow(() -> new RuntimeException("Email verification token not found"));
+  @Transactional
+  public void verifyEmail(String emailVerificationToken) throws MailjetException {
+    log.info("Verifying email with token: {}", emailVerificationToken);
+    Account account =
+        accountsRepository
+            .findByEmailVerificationToken(emailVerificationToken)
+            .orElseThrow(() -> new RuntimeException("Email verification token not found"));
 
-        account.setEmailVerifiedAt(java.time.Instant.now());
-        account.setEmailVerificationToken(null);
-        accountsRepository.save(account);
+    account.setEmailVerifiedAt(java.time.Instant.now());
+    account.setEmailVerificationToken(null);
+    Account savedAccount = accountsRepository.save(account);
+    sendActivationSuccessEmail(savedAccount);
 
-        log.info("Email verified for account with id: {}, username: {}, email: {}",
-                account.getId(),
-                account.getUsername(),
-                account.getEmail());
-    }
+    log.info(
+        "Email verified for account with id: {}, username: {}, email: {}",
+        account.getId(),
+        account.getUsername(),
+        account.getEmail());
+  }
+
+  private void sendActivationSuccessEmail(Account savedAccount) throws MailjetException {
+    mailService.sendTemplate(
+        savedAccount.getUsername(),
+        savedAccount.getEmail(),
+        "activation-success",
+        Map.of("username", savedAccount.getUsername()));
+  }
 }

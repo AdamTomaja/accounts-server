23308e031475bb23279e49beee1cc78c718755c2
cydercode <cydercode@gmail.com>
2026-01-30 02:08:43 +0100

    Spotless java format

--- src/main/java/com/cydercode/exception/RegisterException.java
+++ src/main/java/com/cydercode/exception/RegisterException.java
@@ -2,15 +2,12 @@ package com.cydercode.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @RequiredArgsConstructor
 @Getter
+@ToString
 public class RegisterException extends Exception {
 
   private final RegisterExceptionType type;
-
-  @Override
-  public String toString() {
-    return "RegisterException{" + "type=" + type + '}';
-  }
 }
